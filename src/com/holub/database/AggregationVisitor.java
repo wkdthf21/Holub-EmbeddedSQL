176ebe333fe2de10e17a368d8f1d13f9f9a98d3e
wkdthf21 <wkdthf21@naver.com>
2020-12-04 00:46:46 +0900

    feat: Add Interface for selecting using aggregation functions

---
+++ src/com/holub/database/AggregationVisitor.java
@@ -0,0 +1,29 @@
+package com.holub.database;
+
+import java.util.Collection;
+
+/**
+ * 
+ * @author yesol
+ *
+ *	Aggregation Function Interface Used by Select Statement
+ *  ( count, sum, avg, max, min ... )
+ *  
+ *  Related Classes are defined by Visitor Pattern
+ *  
+ *  For example :
+ *  	select count(*) from name
+ *  
+ * @see SumVisitor
+ * @see CountVisitor
+ *
+ */
+public interface AggregationVisitor {
+	
+	public Table visit(UnmodifiableTable table, Selector where, String sumColumn, Table[] otherTables);
+	public Table visit(UnmodifiableTable table, Selector where, String sumColumn, Collection otherTables);
+	
+	public Table visit(ConcreteTable table, Selector where, String sumColumn, Table[] otherTables);
+	public Table visit(ConcreteTable table, Selector where, String sumColumn, Collection otherTables);
+	
+}
